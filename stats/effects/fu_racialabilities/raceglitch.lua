7b7d511a0cd1b6a80b5260a8b28e9bb8fe01723a
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-08-05 12:55:00 -0400

    glitch are weaker in water

--- stats/effects/fu_racialabilities/raceglitch.lua
+++ stats/effects/fu_racialabilities/raceglitch.lua
@@ -1,4 +1,7 @@
+require("/scripts/vec2.lua")
+
 function init()
+inWater=0
 effect.addStatModifierGroup({{stat = "protection", amount = 7}})
 effect.addStatModifierGroup({{stat = "waterbreathProtection", amount = 1}})
 effect.addStatModifierGroup({{stat = "breathProtection", amount = 1}})
@@ -10,14 +13,47 @@ effect.addStatModifierGroup({{stat = "maxHealth", amount = baseValue }})
 baseValue2 = config.getParameter("energyBonus",0)*(status.resourceMax("energy"))
 effect.addStatModifierGroup({{stat = "maxEnergy", amount = baseValue2 }})
   
-script.setUpdateDelta(0)
+script.setUpdateDelta(5)
 
+end
+
+function isDry()
+local mouthPosition = vec2.add(mcontroller.position(), status.statusProperty("mouthPosition"))
+	if not world.liquidAt(mouthPosition) then
+            status.clearPersistentEffects("glitchweaken")
+            status.clearPersistentEffects("glitchweaken2")
+            status.clearPersistentEffects("glitchweaken3")
+	    inWater = 0
+	end
 end
 
 function update(dt)
+local mouthPosition = vec2.add(mcontroller.position(), status.statusProperty("mouthPosition"))
 
+local mouthful = world.liquidAt(mouthposition)
+local summationForDebug = ""
+
+if mouthful then 
+summationForDebug = "Liq:" .. mouthful .. "/" .. inWater
+else 
+summationForDebug = "Liq:nil/" .. inWater 
+end
+world.debugText(summationForDebug,{mouthPosition[1]-(string.len(summationForDebug)*0.25),mouthPosition[2]},"red")
+
+	if world.liquidAt(mouthPosition) and inWater == 0 then
+            status.setPersistentEffects("glitchweaken", {{stat = "protection", amount = -7}})
+            status.setPersistentEffects("glitchweaken2", {{stat = "maxHealth", amount = -10}})
+            status.setPersistentEffects("glitchweaken3", {{stat = "maxEnergy", amount = -20}})
+	    inWater = 1
+	     
+
+	else
+	  isDry()
+        end 
 end
 
 function uninit()
-  
+              status.clearPersistentEffects("glitchweaken")
+              status.clearPersistentEffects("glitchweaken2")
+              status.clearPersistentEffects("glitchweaken3")
 end
