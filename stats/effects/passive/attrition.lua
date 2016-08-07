7fa6ecf4630e1b9ceb7ab3057d9a90ab5f399dcd
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-08-06 23:34:07 -0400

    stuffs

--- stats/effects/passive/attrition.lua
+++ stats/effects/passive/attrition.lua
@@ -1,13 +1,13 @@
 function init()
   effect.addStatModifierGroup({{stat = "foodDelta", amount = -0.03}})
   effect.addStatModifierGroup({{stat = "fallDamageMultiplier", amount = 1.15}})
+  effect.addStatModifierGroup({{stat = "energyRegenBlockTime", amount = 1.6}})
+  effect.addStatModifierGroup({{stat = "energyRegenPercentageRate", amount = 0.05}})
   script.setUpdateDelta(5)
 end
 
 function update(dt)
-	mcontroller.controlModifiers({
-			speedModifier = 0.90
-		})
+
 end
 
 
