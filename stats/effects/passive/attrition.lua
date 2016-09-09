c748f3abb9194f609a86f1d5b474562d5199286c
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-09-08 22:11:02 -0400

    fix

--- stats/effects/passive/attrition.lua
+++ stats/effects/passive/attrition.lua
@@ -1,5 +1,5 @@
 function init()
-  effect.addStatModifierGroup({{stat = "foodDelta", amount = 0.03}})
+  effect.addStatModifierGroup({{stat = "foodDelta", amount = 0.015}})
   effect.addStatModifierGroup({{stat = "fallDamageMultiplier", amount = 1.10}})
   effect.addStatModifierGroup({{stat = "energyRegenBlockTime", amount = 1.45}})
   effect.addStatModifierGroup({{stat = "energyRegenPercentageRate", amount = 0.05}})
