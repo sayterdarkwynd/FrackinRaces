43639d5cfeaf4dcf1ff1668b13da0c0557706c72
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-10-03 11:15:21 -0400

    slimes dont slow in slime

---
+++ stats/effects/slimeslow/slimeslow.lua
@@ -0,0 +1,28 @@
+function init()
+  animator.setParticleEmitterOffsetRegion("drips", mcontroller.boundBox())
+  animator.setParticleEmitterActive("drips", true)
+  effect.setParentDirectives("fade=347857=0.8")
+end
+
+function update(dt)
+	if world.entitySpecies(entity.id()) == "slimeperson" then
+	self.healingRate = 0.015
+	status.modifyResourcePercentage("health", self.healingRate * dt)
+        
+		  mcontroller.controlModifiers({
+		      groundMovementModifier = 1
+		      speedModifier = 1
+		      airJumpModifier = 1
+		    })
+        else
+		  mcontroller.controlModifiers({
+      			groundMovementModifier = 0.5,
+      			speedModifier = 0.25
+		    })        
+	end    
+    
+end
+
+function uninit()
+
+end
