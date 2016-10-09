2da3130a05a378a3122191fd77649671a3b13d10
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-10-09 18:02:11 -0400

    avian stat effect fix

--- stats/effects/fu_racialabilities/raceavian.lua
+++ stats/effects/fu_racialabilities/raceavian.lua
@@ -1,4 +1,8 @@
 function init()
+baseValue = config.getParameter("healthBonus",0)*(status.resourceMax("health"))
+effect.addStatModifierGroup({{stat = "maxHealth", amount = baseValue }})
+baseValue2 = config.getParameter("energyBonus",0)*(status.resourceMax("energy"))
+effect.addStatModifierGroup({{stat = "maxEnergy", amount = baseValue2 }})
   self.powerModifier = config.getParameter("powerModifier", 0)
   effect.addStatModifierGroup({{stat = "powerMultiplier", baseMultiplier = self.powerModifier}})
   effect.addStatModifierGroup({{stat = "fallDamageMultiplier", baseMultiplier = 0.35}})
