143809fdb6084766fc88b9ad53a80d6e27cfc5f0
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-11-20 09:15:11 -0500

    reduced energy bonus for Novakid
    also addded health /energy values for viera

--- stats/effects/fu_racialabilities/raceviera.lua
+++ stats/effects/fu_racialabilities/raceviera.lua
@@ -3,6 +3,12 @@ effect.addStatModifierGroup({{stat = "shieldStaminaRegen", amount = 0.4}})
 effect.addStatModifierGroup({{stat = "foodDelta", baseMultiplier = 1.06146}})
 effect.addStatModifierGroup({{stat = "slimestickImmunity", amount = 1}})
 effect.addStatModifierGroup({{stat = "slimefrictionImmunity", amount = 1}})
+
+  baseValue = config.getParameter("healthBonus",0)*(status.resourceMax("health"))
+  effect.addStatModifierGroup({{stat = "maxHealth", amount = baseValue }})
+  baseValue2 = config.getParameter("energyBonus",0)*(status.resourceMax("energy"))
+  effect.addStatModifierGroup({{stat = "maxEnergy", amount = baseValue2 }}) 
+  
   local bounds = mcontroller.boundBox()
   script.setUpdateDelta(10)
 end
