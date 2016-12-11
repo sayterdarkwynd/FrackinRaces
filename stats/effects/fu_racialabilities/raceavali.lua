810ae4a09c8677ea616f24ab8b4121bd36c88372
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-12-11 11:46:12 -0500

    fix

--- stats/effects/fu_racialabilities/raceavali.lua
+++ stats/effects/fu_racialabilities/raceavali.lua
@@ -5,7 +5,7 @@ function init()
   effect.addStatModifierGroup({
     {stat = "snowslowImmunity", amount = 1},
     {stat = "biomecoldImmunity", amount = 1},
-    {stat = "iceStatusImmunity", amount = 1}
+    {stat = "iceStatusImmunity", amount = 1},
     {stat = "maxHealth", amount = baseValue },
     {stat = "maxEnergy", amount = baseValue2 },
     {stat = "physicalResistance", amount = 0},
