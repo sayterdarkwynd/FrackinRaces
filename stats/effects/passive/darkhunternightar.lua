3d78c8712dc9295ef3507e649a40184005999590
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-01-13 14:10:43 -0500

    nightar bonus

--- stats/effects/passive/darkhunternightar.lua
+++ stats/effects/passive/darkhunternightar.lua
@@ -21,49 +21,49 @@ function update(dt)
 	  {stat = "maxEnergy", baseMultiplier = config.getParameter("powerBonus",0) + 1.10},
 	  {stat = "powerMultiplier", baseMultiplier = config.getParameter("powerBonus",0) + 1.07}
 	  })		
-	  mcontroller.controlModifiers({ speedModifier = 1.20 }) 
+	  mcontroller.controlModifiers({ speedModifier = 1.16 }) 
 	elseif lightLevel <= 3 then
 	  status.setPersistentEffects("feneroxEffects", {
 	  {stat = "maxHealth", baseMultiplier = config.getParameter("powerBonus",0) + 1.08},
 	  {stat = "maxEnergy", baseMultiplier = config.getParameter("powerBonus",0) + 1.08},
 	  {stat = "powerMultiplier", baseMultiplier = config.getParameter("powerBonus",0) + 1.06}
 	  })		
-	  mcontroller.controlModifiers({ speedModifier = 1.18 }) 	
+	  mcontroller.controlModifiers({ speedModifier = 1.15 }) 	
 	elseif lightLevel <= 5 then
 	  status.setPersistentEffects("feneroxEffects", {
 	  {stat = "maxHealth", baseMultiplier = config.getParameter("powerBonus",0) + 1.07},
 	  {stat = "maxEnergyh", baseMultiplier = config.getParameter("powerBonus",0) + 1.07},
 	  {stat = "powerMultiplier", baseMultiplier = config.getParameter("powerBonus",0) + 1.05}
 	  })		
-	  mcontroller.controlModifiers({ speedModifier = 1.16 }) 
+	  mcontroller.controlModifiers({ speedModifier = 1.14 }) 
 	elseif lightLevel <= 8 then
 	  status.setPersistentEffects("feneroxEffects", {
 	  {stat = "maxHealth", baseMultiplier = config.getParameter("powerBonus",0) + 1.06},
 	  {stat = "maxEnergy", baseMultiplier = config.getParameter("powerBonus",0) + 1.06},
 	  {stat = "powerMultiplier", baseMultiplier = config.getParameter("powerBonus",0) + 1.04}
 	  })		
-	  mcontroller.controlModifiers({ speedModifier = 1.14 }) 	  
+	  mcontroller.controlModifiers({ speedModifier = 1.12 }) 	  
 	elseif lightLevel <= 10 then
 	  status.setPersistentEffects("feneroxEffects", {
 	  {stat = "maxHealth", baseMultiplier = config.getParameter("powerBonus",0) + 1.05},
 	  {stat = "maxEnergy", baseMultiplier = config.getParameter("powerBonus",0) + 1.05},
 	  {stat = "powerMultiplier", baseMultiplier = config.getParameter("powerBonus",0) + 1.02}
 	  })		
-	  mcontroller.controlModifiers({ speedModifier = 1.11 })	
+	  mcontroller.controlModifiers({ speedModifier = 1.09 })	
 	elseif lightLevel <= 15 then
 	  status.setPersistentEffects("feneroxEffects", {
 	  {stat = "maxHealth", baseMultiplier = config.getParameter("powerBonus",0) + 1.04},
 	  {stat = "maxEnergy", baseMultiplier = config.getParameter("powerBonus",0) + 1.04},
 	  {stat = "powerMultiplier", baseMultiplier = config.getParameter("powerBonus",0) + 1.01}
 	  })		
-	  mcontroller.controlModifiers({ speedModifier = 1.09 })	  
+	  mcontroller.controlModifiers({ speedModifier = 1.06 })	  
 	elseif lightLevel <= 25 then
 	  status.setPersistentEffects("feneroxEffects", {
 	  {stat = "maxHealth", baseMultiplier = config.getParameter("powerBonus",0) + 1.02},
 	  {stat = "maxEnergy", baseMultiplier = config.getParameter("powerBonus",0) + 1.02},
 	  {stat = "powerMultiplier", baseMultiplier = config.getParameter("powerBonus",0) + 1.00}
 	  })		
-	  mcontroller.controlModifiers({ speedModifier = 1.06 })
+	  mcontroller.controlModifiers({ speedModifier = 1.04 })
 	else
 	  status.clearPersistentEffects("feneroxEffects")
 	end  
