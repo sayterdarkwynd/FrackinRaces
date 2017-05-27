ccac5574fd01462ed73a1888ec279f380e4456ad
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-05-26 21:05:13 -0400

    adjusted

--- stats/effects/food/robofood.lua
+++ stats/effects/food/robofood.lua
@@ -6,23 +6,32 @@ function init()
   self.tickDamagePercentage = 0.01
   self.tickTime = 2
   self.tickTimer = self.tickTime
-  
   checkRace()
   script.setUpdateDelta(5)
 end
 
 function checkRace()
-  if (world.entitySpecies(entity.id()) == "glitch") or (world.entitySpecies(entity.id()) == "elunite") or (world.entitySpecies(entity.id()) == "trink") or (world.entitySpecies(entity.id()) == "droden") then
+  if (world.entitySpecies(entity.id()) == "glitch") 
+  or (world.entitySpecies(entity.id()) == "elunite") 
+  or (world.entitySpecies(entity.id()) == "trink") 
+  or (world.entitySpecies(entity.id()) == "droden") then
     applyEffects()
-    self.isRace = 1
   else
-    self.isRace = 0 
+    self.isNot = 1
   end
 end
 
+function applyEffects()
+    status.setPersistentEffects("glitchpower1", {
+      {stat = "protection", amount = self.protectionBonus},
+      {stat = "maxHealth", baseMultiplier = baseValue },
+      {stat = "maxEnergy", baseMultiplier = baseValue2 },
+      {stat = "fallDamageMultiplier", baseMultiplier = baseValue3}
+    })
+end
+
 function update(dt)
-  checkRace()
-  if (self.isRace == 0) and (self.tickTimer <= 0) then
+  if (self.isNot == 1) and (self.tickTimer <= 0) then
       self.tickTimer = self.tickTime
       status.applySelfDamageRequest({
         damageType = "IgnoresDef",
@@ -33,15 +42,6 @@ function update(dt)
       effect.setParentDirectives("fade=806e4f="..self.tickTimer * 0.4) 
   end
   self.tickTimer = self.tickTimer - dt
-end
-
-function applyEffects()
-    status.setPersistentEffects("glitchpower1", {
-      {stat = "protection", amount = self.protectionBonus},
-      {stat = "maxHealth", baseMultiplier = baseValue },
-      {stat = "maxEnergy", baseMultiplier = baseValue2 },
-      {stat = "fallDamageMultiplier", baseMultiplier = baseValue3}
-    })
 end
 
 function uninit()
