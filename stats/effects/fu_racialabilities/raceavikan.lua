1d31c9c9a5fd6686d3cbd7e494bba0a7a77f90c8
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-08-06 21:09:41 -0400

    fixes

--- stats/effects/fu_racialabilities/raceavikan.lua
+++ stats/effects/fu_racialabilities/raceavikan.lua
@@ -3,14 +3,11 @@ function init()
   self.baseMaxHealth = status.stat("maxHealth")
   self.baseMaxEnergy = status.stat("maxEnergy")
   effect.addStatModifierGroup({
-    -- base Attributes
     {stat = "isOmnivore", amount = 1},
     {stat = "maxHealth", amount = self.baseMaxHealth * config.getParameter("healthBonus")},
     {stat = "maxEnergy", amount = self.baseMaxEnergy * config.getParameter("energyBonus")},
     {stat = "powerMultiplier", baseMultiplier = config.getParameter("attackBonus")},
-    --{stat = "protection", baseMultiplier = config.getParameter("defenseBonus")},
     {stat = "fallDamageMultiplier", baseMultiplier = config.getParameter("fallBonus")},
-    -- resistances
     {stat = "physicalResistance", amount = config.getParameter("physicalResistance")},
     {stat = "electricResistance", amount = config.getParameter("electricResistance")},
     {stat = "fireResistance", amount = config.getParameter("fireResistance")},
