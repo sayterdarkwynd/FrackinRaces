2ba1700d7c6721cf6e403bd162688b3c2bd059bb
Gamrguy <colmandavenport@gmail.com>
2017-08-24 00:35:04 -0400

    Fixed small bug that probably doesn't even matter

--- interface/scripted/mmupgrade/mmupgradegui.lua
+++ interface/scripted/mmupgrade/mmupgradegui.lua
@@ -108,6 +108,7 @@ function giveRacialManipulator()
         if manip.item then
             local newmm = root.createItem(manip.item)
             newmm.parameters.upgrades = mm.parameters.upgrades
+            newmm.parameters.canCollectLiquid = mm.parameters.canCollectLiquid
             if manip.collectLiquid then
                 newmm.parameters.canCollectLiquid = true
                 table.insert(newmm.parameters.upgrades, "liquidcollection")
@@ -121,7 +122,6 @@ function giveRacialManipulator()
             for _,v in pairs(statsToUpdate) do
                 newpar[v] = updateMMStats(newcfg[v], newpar[v], oldcfg[v], oldpar[v])
             end
-            newpar.canCollectLiquid = oldpar.canCollectLiquid
             newmm.parameters = newpar
 
             player.giveEssentialItem("beamaxe", newmm)
