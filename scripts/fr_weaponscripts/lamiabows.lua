3eb8cd77c7b14263df126183e3c8c9f392eb979f
sayterdarkwynd <sayterdarkwynd@gmail.com>
2017-11-08 14:08:39 -0500

    llamia bow fix

--- scripts/fr_weaponscripts/lamiabows.lua
+++ scripts/fr_weaponscripts/lamiabows.lua
@@ -13,25 +13,25 @@ function FRHelper:call()
     if math.random(10) == 1	then
         local attackType = math.random(6)
         if (attackType) == 1 and (energyValue) >= 100 then
-            params = { power = energyValue/10 , damageKind = "shadow", speed = 45, timeToLive = 3 }
+            params = { power = energyValue/10 , damageKind = "poison", speed = 45, timeToLive = 3 }
             projectileId = world.spawnProjectile("purplearrow",self.script_params:firePosition(),activeItem.ownerEntityId(),self.script_params:aimVector(),false,params)
         elseif (attackType) == 2 and (energyValue) >= 100 then
-            params = { power = energyValue/12 , damageKind = "shadow", speed = 60, timeToLive = 3 }
+            params = { power = energyValue/12 , damageKind = "fire", speed = 60, timeToLive = 3 }
             projectileId = world.spawnProjectile("chargedpurplearrow",self.script_params:firePosition(),activeItem.ownerEntityId(),self.script_params:aimVector(),false,params)
         elseif (attackType) == 3 and (energyValue) >= 100 then
-            params = { power = energyValue/12 , damageKind = "shadow", speed = 30, timeToLive = 1 }
+            params = { power = energyValue/12 , damageKind = "ice", speed = 30, timeToLive = 1 }
             projectileId = world.spawnProjectile("magentaglobelamia",self.script_params:firePosition(),activeItem.ownerEntityId(),self.script_params:aimVector(),false,params)
         elseif (attackType) == 4 and (energyValue) >= 100 then
             params = { power = energyValue/12 , damageKind = "poison", speed = 50, timeToLive = 1 }
             projectileId = world.spawnProjectile("poisonplasma",self.script_params:firePosition(),activeItem.ownerEntityId(),self.script_params:aimVector(),false,params)
         elseif (attackType) == 5 and (energyValue) >= 100 then
-            params = { power = energyValue/12 , damageKind = "shadow", speed = 50, timeToLive = 1 }
+            params = { power = energyValue/12 , damageKind = "electric", speed = 50, timeToLive = 1 }
             projectileId = world.spawnProjectile("ngravitybolt",self.script_params:firePosition(),activeItem.ownerEntityId(),self.script_params:aimVector(),false,params)
         elseif (attackType) == 6 and (energyValue) >= 100 then
-            params = { power = energyValue/12 , damageKind = "shadow", speed = 30, timeToLive = 1 }
+            params = { power = energyValue/12 , damageKind = "ice", speed = 30, timeToLive = 1 }
             projectileId = world.spawnProjectile("orangeglobelamia",self.script_params:firePosition(),activeItem.ownerEntityId(),self.script_params:aimVector(),false,params)
         else
-            params = { power = energyValue/12 , damageKind = "shadow", speed = energyValue/3, timeToLive = 3 }
+            params = { power = energyValue/12 , damageKind = "fire", speed = energyValue/3, timeToLive = 3 }
             projectileId = world.spawnProjectile("purplearrow",self.script_params:firePosition(),activeItem.ownerEntityId(),self.script_params:aimVector(),false,params)
         end
     end
