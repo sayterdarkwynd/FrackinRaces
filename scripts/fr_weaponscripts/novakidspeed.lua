f7b1464b8880edb0d954a998ac622f1f3c200f25
Gamrguy <colmandavenport@gmail.com>
2018-02-11 12:00:45 -0500

    Novakid speed bonus reduces damage proportional to bonus

--- scripts/fr_weaponscripts/novakidspeed.lua
+++ scripts/fr_weaponscripts/novakidspeed.lua
@@ -1,10 +1,12 @@
 function FRHelper:call(args, main, ...)
     local lightLevel = getLight()
-    local speedMod = 1.0 - (lightLevel / 200)
+    local speedMod = (args.minSpeed or 1) - (lightLevel / (args.lightValue or 200))
     if select(1, ...) and type(select(1, ...)) == "number" then
         main.cooldownTimer = main.cooldownTimer * speedMod
+        self:applyStats({ stats={ {stat="powerMultiplier", multiplier=1 - (1 - speedMod) * 0.5} } }, "FR_novakidSpeedPenalty")
     elseif main.fireType == "burst" then
         main.cooldownTimer = main.cooldownTimer * speedMod
+        self:applyStats({ stats={ {stat="powerMultiplier", multiplier=1 - (1 - speedMod) * 0.5} } }, "FR_novakidSpeedPenalty")
     else
         main.cooldownTimer = main.fireTime * speedMod
     end
