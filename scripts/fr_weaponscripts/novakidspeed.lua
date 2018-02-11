aef05635537516e9dcb3c9ccd830e5478d2d7d2b
Gamrguy <colmandavenport@gmail.com>
2018-02-11 13:14:11 -0500

    tweak

--- scripts/fr_weaponscripts/novakidspeed.lua
+++ scripts/fr_weaponscripts/novakidspeed.lua
@@ -1,12 +1,10 @@
 function FRHelper:call(args, main, ...)
     local lightLevel = getLight()
     local speedMod = (args.minSpeed or 1) - (lightLevel / (args.lightValue or 200))
-    if select(1, ...) and type(select(1, ...)) == "number" then
+    if (select(1, ...) and type(select(1, ...)) == "number") or main.fireType == "burst" then
         main.cooldownTimer = main.cooldownTimer * speedMod
-        self:applyStats({ stats={ {stat="powerMultiplier", baseMultiplier=1 - (1 - speedMod) * 0.5} } }, "FR_novakidSpeedPenalty")
-    elseif main.fireType == "burst" then
-        main.cooldownTimer = main.cooldownTimer * speedMod
-        self:applyStats({ stats={ {stat="powerMultiplier", baseMultiplier=1 - (1 - speedMod) * 0.5} } }, "FR_novakidSpeedPenalty")
+        local x = math.max(math.min(1 - (1 - speedMod) * 0.5, args.damageCeiling or 1), args.damageFloor or 0.5)
+        self:applyStats({ stats={ {stat="powerMultiplier", baseMultiplier=x} } }, "FR_novakidSpeedPenalty")
     else
         main.cooldownTimer = main.fireTime * speedMod
     end
