896841eb6ce1138659da21bb70d7c6763a956c30
sayterdarkwynd <sayterdarkwynd@gmail.com>
2018-02-20 21:45:22 -0500

    radiens

--- stats/bonuses/radienFoodBoost.lua
+++ stats/bonuses/radienFoodBoost.lua
@@ -1,46 +1,53 @@
-
-function update(dt)
+function foodCheck()
   if status.isResource("food") then
     self.foodValue = status.resource("food")
   else
     self.foodValue = 30
   end
-  
   self.foodValue = self.foodValue / 100
-  self.foodValueBase = self.foodValue  -- store value unmodified
+end
+
+function setHealth()
+  self.baseHealth = 0.95
+  self.baseEnergy = 1.1
   
-  self.radiationBoost = self.foodValue / 1.4
-  self.poisonValueBonus = self.foodValue /2
-  self.powerMultBonus = self.foodValue /3.3
-  self.healthMultPenalty = self.foodValue /4
-  
-  if self.foodValueBase < 35 then  --failsafes
+  if self.foodValue > 0.35 then  
+    self.healthMultPenalty = self.baseHealth * ( self.baseHealth *  -(1 - (self.foodValue))) 
+    self.healthMultPenalty = math.floor(self.healthMultPenalty * 10)  
+    self.energyMultBonus = self.baseEnergy * ( self.baseEnergy *  -(1 - (self.foodValue)))
+    self.energyMultBonus = math.floor(self.energyMultBonus * 10)
+  else
+    self.healthMultPenalty = -10
+    self.energyMultBonus = -11
+  end
+end
+
+function update(dt)
+  foodCheck()
+  setHealth()
+
+  self.radiationBoost = self.foodValue / 1.4 
+  self.poisonValueBonus = self.foodValue /2.8 
+  self.powerMultBonus = self.foodValue /4.6
+
+  --failsafes so that at 50% food you are hard-locked to a particular amount to not get too weak
+  if self.foodValue < 0.35 then  
       self.firePenaltyBonusMod = -0.4
-      self.foodValue = 0.25
       self.poisonValueBonus = 0.185
       self.powerMultBonus = 1.0   
-      self.healthMultPenalty = 0.75
   else
       self.firePenaltyBonusMod = -0.4 + (self.powerMultBonus)
   end
   
-  if self.foodValueBase < 65 then  -- only negatively affect them after a certain percentage is gone
-        status.setPersistentEffects("radienPower", {
+
+        status.setPersistentEffects("radienPower", {  
             {stat = "maxHealth", amount = self.healthMultPenalty  },
-            {stat = "maxEnergy", amount = self.powerMultBonus },         
-            {stat = "powerMultiplier", baseMultiplier = 1 + self.powerMultBonus },
+            {stat = "maxEnergy", amount = self.energyMultBonus },             
+            {stat = "powerMultiplier", baseMultiplier = 1 + self.powerMultBonus},
             {stat = "radioactiveResistance", amount = self.radiationBoost },
-            {stat = "poisonResistance", amount = self.poisonValueBonus },
-            {stat = "fireResistance", amount = self.firePenaltyBonusMod }
-        })    
-  else
-        status.setPersistentEffects("radienPower", {   
-            {stat = "powerMultiplier", baseMultiplier = 1.15 },
-            {stat = "radioactiveResistance", amount = 0.5 },
-            {stat = "poisonResistance", amount = 0.25},
+            {stat = "poisonResistance", amount = self.poisonValueBonus},
             {stat = "fireResistance", amount = self.firePenaltyBonusMod }
         })  
-  end
   
 end
 
