dffd3156a1e6f5abc00aa1a428738ccfbed64907
sayterdarkwynd <sayterdarkwynd@gmail.com>
2018-02-21 11:37:25 -0500

    fafsafasas

--- stats/bonuses/radienFoodBoost.lua
+++ stats/bonuses/radienFoodBoost.lua
@@ -2,59 +2,65 @@ function foodCheck()
   if status.isResource("food") then
     self.foodValue = status.resource("food")
   else
-    self.foodValue = 40
-  end
-  self.foodValue = self.foodValue / 100
-end
-
-function setHealthTier()
-  --3 tiers of hunger, plus the Full state
-  self.baseMod = 1
-  if self.foodValue <= 0.7 then
-    self.baseMod = 1
-  elseif self.foodValue <= 0.699 then
-    self.baseMod = 50    
-  elseif self.foodValue < 0.467 then
-    self.baseMod = 55
-  elseif self.foodValue < 0.234 then
-    self.baseMod = 60
+    self.foodValue = 35
   end
   
-  self.baseHealth = 0.95
-  self.baseEnergy = 1.1
-  self.healthMultPenalty = (self.baseHealth * ( self.baseHealth *  -(1 - (self.foodValue))) )* self.baseMod 
-  self.energyMultBonus = (self.baseEnergy * ( self.baseEnergy *  -(1 - (self.foodValue))) )* self.baseMod 
+  self.foodTier = self.foodValue
+  self.foodValue = self.foodValue / 100
 
 end
 
-function update(dt)
-  foodCheck()
-  setHealthTier()
-
-sb.logInfo("health = "..self.healthMultPenalty)
-sb.logInfo("energy = "..self.energyMultBonus)
+function setValues()
   self.radiationBoost = self.foodValue / 1.4 
   self.poisonValueBonus = self.foodValue /2.8 
   self.powerMultBonus = self.foodValue /4.7
   self.firePenaltyBonusMod = -0.4 + (self.powerMultBonus)
-  
   --failsafes so that at 50% food you are hard-locked to a particular amount to not get too weak
   if self.foodValue < 0.35 then  
       self.firePenaltyBonusMod = -0.4
       self.poisonValueBonus = 0.185
-      self.powerMultBonus = 1.0   
+      self.powerMultBonus = 0 
+      self.radiationBoost = 0.25
+  end
+
+  status.setPersistentEffects("radienPower", {  
+      {stat = "maxHealth", amount = self.healthMultPenalty * self.baseMod },
+      {stat = "maxEnergy", amount = self.energyMultBonus * self.baseMod },             
+      {stat = "powerMultiplier", baseMultiplier = 1 + self.powerMultBonus},
+      {stat = "radioactiveResistance", amount = self.radiationBoost },
+      {stat = "poisonResistance", amount = self.poisonValueBonus},
+      {stat = "fireResistance", amount = self.firePenaltyBonusMod }
+  }) 
+end
+
+function checkHealth()
+  self.baseHealth = 0.95
+  self.baseEnergy = 1.1
+  self.healthMultPenalty = self.baseHealth * ( self.baseHealth *  -(1 - (self.foodValue))) 
+  self.energyMultBonus = self.baseEnergy * ( self.baseEnergy *  -(1 - (self.foodValue)))  
+  self.finalHealth = math.ceil(self.healthMultPenalty * self.baseMod) +0.5
+  self.finalEnergy = math.ceil(self.healthMultPenalty * self.baseMod) +0.5
+end
+
+function update(dt)
+  foodCheck()  -- check if they have food bar or not
+  sb.logInfo(" food =  "..self.foodValue)
+  if self.foodValue >= 0.69970657348633 then
+    self.dt = 1
+    self.baseMod = 1
+  
+  else
+    self.dt = 0
+    self.baseMod = 1 * (10 + self.dt)  
+    self.dt = dt + (self.baseMod)   
   end
   
 
-        status.setPersistentEffects("radienPower", {  
-            {stat = "maxHealth", amount = (self.baseHealth * ( self.baseHealth *  -(1 - (self.foodValue))) )* self.baseMod  },
-            {stat = "maxEnergy", amount = (self.baseEnergy * ( self.baseEnergy *  -(1 - (self.foodValue))) )* self.baseMod },             
-            {stat = "powerMultiplier", baseMultiplier = 1 + self.powerMultBonus},
-            {stat = "radioactiveResistance", amount = self.radiationBoost },
-            {stat = "poisonResistance", amount = self.poisonValueBonus},
-            {stat = "fireResistance", amount = self.firePenaltyBonusMod }
-        })  
+
   
+  checkHealth()  -- set health adjustment stats 
+  setValues() -- set adjustments to stats
+
 end
 
 function uninit()
