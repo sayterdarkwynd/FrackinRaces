9401a57009d1dc8051d07ac81a06c09390e2c038
sayterdarkwynd <sayterdarkwynd@gmail.com>
2018-05-17 12:29:44 -0400

    I LIKE CHEETOS

--- stats/bonuses/lastreeFoodBonus.lua
+++ stats/bonuses/lastreeFoodBonus.lua
@@ -2,7 +2,7 @@ function update(dt)
   self.healingRateGood = 0.015
   if status.isResource("food") then
     self.foodValue = status.resource("food")
-    if self.foodValue > = 35 then
+    if self.foodValue >= 35 then
       status.modifyResourcePercentage("health", self.healingRateGood * dt)
     end
   end
