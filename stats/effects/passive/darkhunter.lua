2cb1f4c42a00631efba51fe78f04840453122b54
Kherae <cwaldridge@live.com>
2018-07-19 06:27:10 -0700

    darkhunter stat mod switch

--- stats/effects/passive/darkhunter.lua
+++ stats/effects/passive/darkhunter.lua
@@ -1,6 +1,7 @@
 function init()
+	darkHunterEffects=effect.addStatModifierGroup({})
   local bounds = mcontroller.boundBox()
-  self.powerBonus = 0.1
+  self.powerBonus = config.getParameter("powerBonus",0)
   script.setUpdateDelta(10)
 end
 
@@ -16,59 +17,59 @@ end
 function update(dt)
   local lightLevel = getLight()
 	if lightLevel <= 1 then
-	  status.setPersistentEffects("feneroxEffects", {
-	  {stat = "energyRegenPercentageRate", amount = config.getParameter("powerBonus",0)},
-	  {stat = "maxHealth", baseMultiplier = config.getParameter("powerBonus",0) + 1.08},
-	  {stat = "powerMultiplier", baseMultiplier = config.getParameter("powerBonus",0) + 1.08}
+	  status.setStatModifierGroup(darkHunterEffects, {
+	  {stat = "energyRegenPercentageRate", amount = self.powerBonus},
+	  {stat = "maxHealth", baseMultiplier = self.powerBonus + 1.08},
+	  {stat = "powerMultiplier", baseMultiplier = self.powerBonus + 1.08}
 	  })		
 	  mcontroller.controlModifiers({ speedModifier = 1.20 }) 
 	elseif lightLevel <= 3 then
-	  status.setPersistentEffects("feneroxEffects", {
-	  {stat = "energyRegenPercentageRate", amount = config.getParameter("powerBonus",0)},
-	  {stat = "maxHealth", baseMultiplier = config.getParameter("powerBonus",0) + 1.07},
-	  {stat = "powerMultiplier", baseMultiplier = config.getParameter("powerBonus",0) + 1.07}
+	  status.setStatModifierGroup(darkHunterEffects, {
+	  {stat = "energyRegenPercentageRate", amount = self.powerBonus},
+	  {stat = "maxHealth", baseMultiplier = self.powerBonus + 1.07},
+	  {stat = "powerMultiplier", baseMultiplier = self.powerBonus + 1.07}
 	  })		
 	  mcontroller.controlModifiers({ speedModifier = 1.18 }) 	
 	elseif lightLevel <= 5 then
-	  status.setPersistentEffects("feneroxEffects", {
-	  {stat = "energyRegenPercentageRate", amount = config.getParameter("powerBonus",0)},
-	  {stat = "maxHealth", baseMultiplier = config.getParameter("powerBonus",0) + 1.06},
-	  {stat = "powerMultiplier", baseMultiplier = config.getParameter("powerBonus",0) + 1.06}
+	  status.setStatModifierGroup(darkHunterEffects, {
+	  {stat = "energyRegenPercentageRate", amount = self.powerBonus},
+	  {stat = "maxHealth", baseMultiplier = self.powerBonus + 1.06},
+	  {stat = "powerMultiplier", baseMultiplier = self.powerBonus + 1.06}
 	  })		
 	  mcontroller.controlModifiers({ speedModifier = 1.16 }) 
 	elseif lightLevel <= 8 then
-	  status.setPersistentEffects("feneroxEffects", {
-	  {stat = "energyRegenPercentageRate", amount = config.getParameter("powerBonus",0)},
-	  {stat = "maxHealth", baseMultiplier = config.getParameter("powerBonus",0) + 1.05},
-	  {stat = "powerMultiplier", baseMultiplier = config.getParameter("powerBonus",0) + 1.05}
+	  status.setStatModifierGroup(darkHunterEffects, {
+	  {stat = "energyRegenPercentageRate", amount = self.powerBonus},
+	  {stat = "maxHealth", baseMultiplier = self.powerBonus + 1.05},
+	  {stat = "powerMultiplier", baseMultiplier = self.powerBonus + 1.05}
 	  })		
 	  mcontroller.controlModifiers({ speedModifier = 1.14 }) 	  
 	elseif lightLevel <= 10 then
-	  status.setPersistentEffects("feneroxEffects", {
-	  {stat = "energyRegenPercentageRate", amount = config.getParameter("powerBonus",0)},
-	  {stat = "maxHealth", baseMultiplier = config.getParameter("powerBonus",0) + 1.04},
-	  {stat = "powerMultiplier", baseMultiplier = config.getParameter("powerBonus",0) + 1.04}
+	  status.setStatModifierGroup(darkHunterEffects, {
+	  {stat = "energyRegenPercentageRate", amount = self.powerBonus},
+	  {stat = "maxHealth", baseMultiplier = self.powerBonus + 1.04},
+	  {stat = "powerMultiplier", baseMultiplier = self.powerBonus + 1.04}
 	  })		
 	  mcontroller.controlModifiers({ speedModifier = 1.11 })	
 	elseif lightLevel <= 15 then
-	  status.setPersistentEffects("feneroxEffects", {
-	  {stat = "energyRegenPercentageRate", amount = config.getParameter("powerBonus",0)},
-	  {stat = "maxHealth", baseMultiplier = config.getParameter("powerBonus",0) + 1.02},
-	  {stat = "powerMultiplier", baseMultiplier = config.getParameter("powerBonus",0) + 1.02}
+	  status.setStatModifierGroup(darkHunterEffects, {
+	  {stat = "energyRegenPercentageRate", amount = self.powerBonus},
+	  {stat = "maxHealth", baseMultiplier = self.powerBonus + 1.02},
+	  {stat = "powerMultiplier", baseMultiplier = self.powerBonus + 1.02}
 	  })		
 	  mcontroller.controlModifiers({ speedModifier = 1.09 })	  
 	elseif lightLevel <= 25 then
-	  status.setPersistentEffects("feneroxEffects", {
-	  {stat = "energyRegenPercentageRate", amount = config.getParameter("powerBonus",0)},
-	  {stat = "maxHealth", baseMultiplier = config.getParameter("powerBonus",0) + 1.00},
-	  {stat = "powerMultiplier", baseMultiplier = config.getParameter("powerBonus",0) + 1.00}
+	  status.setStatModifierGroup(darkHunterEffects, {
+	  {stat = "energyRegenPercentageRate", amount = self.powerBonus},
+	  {stat = "maxHealth", baseMultiplier = self.powerBonus + 1.00},
+	  {stat = "powerMultiplier", baseMultiplier = self.powerBonus + 1.00}
 	  })		
 	  mcontroller.controlModifiers({ speedModifier = 1.06 })
 	else
-	  status.clearPersistentEffects("feneroxEffects")
+	  status.setStatModifierGroup(darkHunterEffects,{})
 	end  
 end
 
 function uninit()
-  status.clearPersistentEffects("feneroxEffects")
+  status.removeStatModifierGroup(darkHunterEffects)
 end
