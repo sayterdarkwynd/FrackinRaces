1f5012ca4a1613f14d12a3c644b5ccfbaefc4617
Kherae <cwaldridge@live.com>
2018-07-19 03:20:30 -0700

    lighthunter fix

--- stats/effects/passive/lighthunter.lua
+++ stats/effects/passive/lighthunter.lua
@@ -23,42 +23,42 @@ function update(dt)
 	  })		
 	  mcontroller.controlModifiers({ speedModifier = 1.20 }) 
 	elseif lightLevel >= 80 then
-	  status.setPersistentEffects("feneroxEffects", {
+	  status.setPersistentEffects("gardevanEffects", {
 	  {stat = "energyRegenPercentageRate", amount = 1.08 + config.getParameter("powerBonus",0)},
 	  {stat = "maxHealth", baseMultiplier = config.getParameter("powerBonus",0) + 1.07},
 	  {stat = "powerMultiplier", baseMultiplier = config.getParameter("powerBonus",0) + 1.07}
 	  })		
 	  mcontroller.controlModifiers({ speedModifier = 1.18 }) 	
 	elseif lightLevel >= 70 then
-	  status.setPersistentEffects("feneroxEffects", {
+	  status.setPersistentEffects("gardevanEffects", {
 	  {stat = "energyRegenPercentageRate", amount = 1.08 + config.getParameter("powerBonus",0)},
 	  {stat = "maxHealth", baseMultiplier = config.getParameter("powerBonus",0) + 1.06},
 	  {stat = "powerMultiplier", baseMultiplier = config.getParameter("powerBonus",0) + 1.06}
 	  })		
 	  mcontroller.controlModifiers({ speedModifier = 1.16 }) 
 	elseif lightLevel >= 60 then
-	  status.setPersistentEffects("feneroxEffects", {
+	  status.setPersistentEffects("gardevanEffects", {
 	  {stat = "energyRegenPercentageRate", amount = 1.08 + config.getParameter("powerBonus",0)},
 	  {stat = "maxHealth", baseMultiplier = config.getParameter("powerBonus",0) + 1.05},
 	  {stat = "powerMultiplier", baseMultiplier = config.getParameter("powerBonus",0) + 1.05}
 	  })		
 	  mcontroller.controlModifiers({ speedModifier = 1.14 }) 	  
 	elseif lightLevel >= 50 then
-	  status.setPersistentEffects("feneroxEffects", {
+	  status.setPersistentEffects("gardevanEffects", {
 	  {stat = "energyRegenPercentageRate", amount = 1.08 + config.getParameter("powerBonus",0)},
 	  {stat = "maxHealth", baseMultiplier = config.getParameter("powerBonus",0) + 1.04},
 	  {stat = "powerMultiplier", baseMultiplier = config.getParameter("powerBonus",0) + 1.04}
 	  })		
 	  mcontroller.controlModifiers({ speedModifier = 1.11 })	
 	elseif lightLevel >= 40 then
-	  status.setPersistentEffects("feneroxEffects", {
+	  status.setPersistentEffects("gardevanEffects", {
 	  {stat = "energyRegenPercentageRate", amount = 1.08 + config.getParameter("powerBonus",0)},
 	  {stat = "maxHealth", baseMultiplier = config.getParameter("powerBonus",0) + 1.02},
 	  {stat = "powerMultiplier", baseMultiplier = config.getParameter("powerBonus",0) + 1.02}
 	  })		
 	  mcontroller.controlModifiers({ speedModifier = 1.09 })	  
 	elseif lightLevel >= 30 then
-	  status.setPersistentEffects("feneroxEffects", {
+	  status.setPersistentEffects("gardevanEffects", {
 	  {stat = "energyRegenPercentageRate", amount = 1.08 + config.getParameter("powerBonus",0)},
 	  {stat = "maxHealth", baseMultiplier = config.getParameter("powerBonus",0) + 1.00},
 	  {stat = "powerMultiplier", baseMultiplier = config.getParameter("powerBonus",0) + 1.00}
