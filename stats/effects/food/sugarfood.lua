00caf0a95f52c8011ff32d0c5d1072871e010317
Vetpetmon-Labs <vetpetmon@hotmail.com>
2018-07-30 20:01:27 -0400

    sugar speed boost

--- stats/effects/food/sugarfood.lua
+++ stats/effects/food/sugarfood.lua
@@ -3,6 +3,11 @@ function init()
 end
 --status.statPositive(`String` statName)
 function update(dt)
+  if status.stat("isSugar")==1 then
+    mcontroller.controlModifiers({
+      speedModifier = 1.1,
+    })
+  end
 end
 
 function uninit()
