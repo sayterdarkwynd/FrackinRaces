09e127f1d4d37820459194f5f650f684ff37edd9
sayterdarkwynd <sayterdarkwynd@gmail.com>
2018-08-18 09:21:22 -0400

    flight tests

--- stats/effects/passive/foodcost.lua
+++ stats/effects/passive/foodcost.lua
@@ -1,7 +1,20 @@
 function init() 
-effect.addStatModifierGroup({
-  {stat = "foodDelta", amount = -config.getParameter("foodCost", 4)}
-})
+
+  self.species = world.entitySpecies(entity.id())
+  
+  if self.species == "avian" then
+    self.foodCost = 3
+  elseif self.species == "avali" then
+    self.foodCost = 5
+  elseif self.species == "saturnian" then
+    self.foodCost = 8
+  else
+    self.foodCost = 3
+  end
+  
+  effect.addStatModifierGroup({
+    {stat = "foodDelta", amount = -self.foodCost }
+  })
 end
 
 function update(dt)
--- tech/other/flighttest.lua
+++ tech/other/flighttest.lua
@@ -4,38 +4,60 @@ function init()
   self.energyCostPerSecond = config.getParameter("energyCostPerSecond")
   self.active=false
   self.available = true
+  self.species = world.entitySpecies(entity.id())
 end
 
 function uninit()
-
+  animator.setParticleEmitterActive("feathers", false)	
+  animator.stopAllSounds("activate")
+  effect.removeStatModifierGroup(foodPower)
 end
 
-function update(args)
-
+function checkFood()
 	if status.isResource("food") then
-		self.foodValue = status.resource("food")
+		self.foodValue = status.resource("food")		
 	else
 		self.foodValue = 15
 	end
-	
-	
-	if args.moves["special1"] and not mcontroller.onGround() and not mcontroller.zeroG() then 
-	    if config.getParameter("removesFallDamage",0) == 1 then
-	        status.addEphemeralEffects{{effect = "nofalldamage", duration = 0.15}}
-	    end
-	    
-	    if self.foodValue > 15 then 
-	        status.addEphemeralEffects{{effect = "foodcost", duration = 0.15}}
-	    else
-                status.overConsumeResource("energy", config.getParameter("energyCostPerSecond"),1)
-	    end
-	    
-      	    mcontroller.controlParameters(config.getParameter("fallingParameters"))
-      	    mcontroller.setYVelocity(math.max(mcontroller.yVelocity(), config.getParameter("maxFallSpeed")))	    
+end
+
+function activeFlight()
+    if config.getParameter("removesFallDamage",0) == 1 then
+	status.addEphemeralEffects{{effect = "nofalldamage", duration = 0.1}}
+    end
+
+    mcontroller.controlParameters(config.getParameter("fallingParameters"))
+    mcontroller.setYVelocity(math.max(mcontroller.yVelocity(), config.getParameter("maxFallSpeed")))
+    animateFlight()
+end
+
+function animateFlight()
+    if self.species == "saturnian" then
+    	animator.setParticleEmitterOffsetRegion("butterflies", mcontroller.boundBox())
+    	animator.setParticleEmitterActive("butterflies", true)	    
+    else
+    	animator.setParticleEmitterOffsetRegion("feathers", mcontroller.boundBox())
+    	animator.setParticleEmitterActive("feathers", true)	
+    end
+    animator.playSound("activate",2)
+    animator.setFlipped(mcontroller.facingDirection() < 0)   
+end
+
+function update(args)
+        checkFood()
+
+	if args.moves["special1"] and not mcontroller.onGround() and not mcontroller.zeroG() and status.overConsumeResource("energy", 0.001) then 
+		if self.foodValue > 15 then
+		    status.addEphemeralEffects{{effect = "foodcost", duration = 0.1}}
+		else
+		    status.overConsumeResource("energy", config.getParameter("energyCostPerSecond"),1)
+		end	
+	    	activeFlight()
+	else
+  	    animator.setParticleEmitterActive("feathers", false)
+  	    animator.setParticleEmitterActive("butterflies", false)
+  	    animator.stopAllSounds("activate")	
 	end
-  	animator.setParticleEmitterOffsetRegion("feathers", mcontroller.boundBox())
-  	animator.setParticleEmitterActive("feathers", true)	
-	animator.setFlipped(mcontroller.facingDirection() < 0)
 end
 
 function idle()
