81938ee20cd1a35f025abf6fd897e75e1e8f0043
Kherae <cwaldridge@live.com>
2020-04-23 17:41:47 -0700

    generic stat scripts begin

--- stats/effects/swimboost/swimboost1.lua
+++ stats/effects/swimboost/swimboost1.lua
@@ -1,10 +1,10 @@
 function init()
-  effect.addStatModifierGroup({{stat = "boostAmount", amount = 1}}) -- make sure to reset the value to 1 each time it inits so values never stack
+	sb.logInfo("status.stat(boostAmount):%s",status.stat("boostAmount"))
+  --[[effect.addStatModifierGroup({{stat = "boostAmount", amount = 1}}) -- make sure to reset the value to 1 each time it inits so values never stack
   self.mouthPosition = status.statusProperty("mouthPosition") or {0,0}
-  self.mouthBounds = {self.mouthPosition[1], self.mouthPosition[2], self.mouthPosition[1], self.mouthPosition[2]}
+  --self.mouthBounds = {self.mouthPosition[1], self.mouthPosition[2], self.mouthPosition[1], self.mouthPosition[2]}
   self.boostAmount = config.getParameter("boostAmount", 1)
-  effect.addStatModifierGroup({{stat = "boostAmount", effectiveMultiplier = self.boostAmount}}) -- add the swim boost stat
-  --sb.logInfo("ive applied swimboost of x"..status.stat("boostAmount"))
+  effect.addStatModifierGroup({{stat = "boostAmount", effectiveMultiplier = self.boostAmount}}) -- add the swim boost stat]]
 end
 
 function update(dt)
